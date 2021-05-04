c27d81cc06ce6cdfe52d705a5d707c9798494f58
Vikash-kumar0835 <vikash.kumar.phe16@itbhu.ac.in>
2021-05-04 13:33:04 +0530

    JSON-Notation

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
-app.use("/public", express.static(__dirname + "/public"));
+//app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
+  });
+  
 
 
 
